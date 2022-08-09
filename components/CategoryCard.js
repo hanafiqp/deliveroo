abb1c10f1f97965dada790738e9d88ad64deef54
SPCWIT011\Administrator <hpraditia@gmail.com>
2022-08-09 16:17:45 +0700

    Create Restaurant Screen

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -6,7 +6,7 @@ const CategoryCard = ({imgUrl, title}) => {
   return (
     <TouchableOpacity className="relative mr-2">
       <Image
-        source={{uri: urlFor(imgUrl).url()}}
+        source={{uri: urlFor(imgUrl).width(200).url()}}
         className="h-20 w-20 rounded"
       />
       <Text className="absolute bottom-1 left-1 text-white font-bold">
